899faddf43840e856aa5dbc9645bdac5d5d588c9
lucaf1990 <117093377+lucaf1990@users.noreply.github.com>
2023-02-27 21:47:27 +0100

    zs

--- src/Components/ProfileSection.tsx
+++ src/Components/ProfileSection.tsx
@@ -1,4 +1,4 @@
-import { Container } from "react-bootstrap";
+import { Container, Button } from "react-bootstrap";
 import Col from "react-bootstrap/esm/Col";
 import Row from "react-bootstrap/esm/Row";
 import { useSelector } from "react-redux";
@@ -10,34 +10,47 @@ const ProfileSection = () => {
 
   return (
     <Container>
-      <img
-        src={data.image}
-        alt={data.name}
-        style={{
-          height: "160px",
-          width: "160px",
-          padding: "0",
-          borderRadius: "100%",
-          border: "4px solid ",
-          position: "relative",
-          top: "300px",
-          left: "50px",
-        }}
-      />
+      <div style={{ position: "absolute" }}>
+        <img
+          src={data.image}
+          alt={data.name}
+          style={{
+            height: "130px",
+            width: "130px",
+            padding: "0",
+            borderRadius: "100%",
+            border: "4px white solid ",
+            position: "relative",
+            top: "110px",
+            left: "150px",
+          }}
+        />
+      </div>
       <Row id="profileProfile" className="flex-column">
         <Col>
           <img
             src={data.image}
             alt={data.name}
-            style={{ height: "300px", width: "100%" }}
+            style={{ height: "200px", width: "100%" }}
           />
         </Col>
-        <Col id="profileCol">
-          <h3>
+        <Col id="profileCol" className="mt-5">
+          <h6 style={{ fontWeight: "bold" }}>
             {data.name} {data.surname}
-          </h3>
-          <h5>{data.title}</h5>
-          <p>{data.area}</p>
+          </h6>
+          <p style={{ margin: "0", fontSize: "0.8em" }}>{data.title}</p>
+          <p style={{ margin: "0", marginBottom: "1rem", fontSize: "0.8em" }}>
+            {data.area}
+          </p>
+        </Col>
+        <Col>
+          <Button className="Profile-Btn1">Collegati</Button>
+          <Button className="Profile-Btn2 btn-light text-primary">
+            Messaggio
+          </Button>
+          <Button className="Profile-Btn3 btn-light text-secondary">
+            Altro
+          </Button>
         </Col>
       </Row>
     </Container>
