c864cc3d1ab5476dacbcaef6f6e59982b8dfa3be
kevin fuccio <kevin.fuccio00@gmai.com>
2023-02-28 15:50:13 +0100

    added experience (need to fix date)

--- src/Components/ProfileComponent/Experience.tsx
+++ src/Components/ProfileComponent/Experience.tsx
@@ -1,27 +1,39 @@
 import { useSelector } from "react-redux";
 import { RootState } from "../../Redux/Store";
-import { Container,Row,Col } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 const Experience = () => {
-    const mystate = useSelector((state: RootState) => state.profile.profilesFetch);
-    console.log(mystate)
-    return (
-      <Container>
-        <Row id="ActivityProfile">
-          <Col style={{ padding: "0" }}>
-            <Col id="ActivityCol">
-              <h5>Experience</h5>
-            </Col>
-            
-          
-        {mystate.map((e,i)=>(
-          <Col  key={i}>
-              <p>{e.company}</p>
-          </Col>
-        ))}
-        </Col>
-        </Row>
-      </Container>
+
+  const mystate = useSelector(
+    (state: RootState) => state.profile.profilesFetch
     );
-  };
-  
-  export default Experience;
+    console.log(mystate);
+
+    const moment = require('moment');
+    let now = moment().format();
+    
+  return (
+    <Container>
+      <Row id="ActivityProfile">
+        <Col style={{ padding: "0" }}>
+          <Col id="ActivityCol">
+            <h5>Experience</h5>
+          </Col>
+
+          {mystate.map((e, i) => (
+            <Col key={i} className='mb-4'>
+              <h5>{e.role}</h5>
+              <div>
+              <span>{e.company}</span>
+              <span style={{display:'block'}}>{}</span>
+              <span>{e.area}</span>
+              </div>
+              <div>{e.description}</div>
+            </Col>
+          ))}
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default Experience;
