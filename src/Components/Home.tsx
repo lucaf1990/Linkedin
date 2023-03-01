1f93538a303108bc7e26d97f73e99767ea87374a
bebob94 <bebo.macis@gmail.com>
2023-03-01 18:20:46 +0100

    add

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -1,13 +1,13 @@
 import Feed from "./NewsComponents/Feed";
 import NewsSideBar from "./NewsComponents/NewsSideBar";
-import SideBar from "./ProfileComponent/SideBar";
+import RightBar from "./NewsComponents/rightCard";
 
 function Home() {
   return (
     <div className="d-flex mt-3">
       <NewsSideBar />
       <Feed />
-      <SideBar />
+      <RightBar />
     </div>
   );
 }
