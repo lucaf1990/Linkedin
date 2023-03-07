6ce3c761a93df865f1fa7db60487df8793b1b0ad
lucaf1990 <117093377+lucaf1990@users.noreply.github.com>
2023-03-07 17:29:06 +0100

    K

---
+++ src/Components/Home.tsx
@@ -0,0 +1,52 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  fetchMyProfile,
+  fetchProfiles,
+  ME,
+  PROFILES_FETCH,
+} from "../Redux/ActionTypes";
+import { fetchHome, HOME_FETCH } from "../Redux/ActionTypes/homeAction";
+import { RootState } from "../Redux/Store";
+
+import Feed from "./NewsComponents/Feed";
+import NewsSideBar from "./NewsComponents/NewsSideBar";
+import RightBar from "./NewsComponents/rightCard";
+
+function Home() {
+  const myState = useSelector((state: RootState) => state.profile.me);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    (async () => {
+      let data = await fetchMyProfile("me");
+      dispatch({
+        type: ME,
+        payload: data,
+      });
+      let data3 = await fetchProfiles();
+      dispatch({
+        type: PROFILES_FETCH,
+        payload: data3,
+      });
+    })();
+  }, []);
+  useEffect(() => {
+    (async () => {
+      let data2 = await fetchHome();
+      dispatch({
+        type: HOME_FETCH,
+        payload: data2,
+      });
+    })();
+  }, []);
+
+  return (
+    <div className="d-flex mt-3 justify-content-center">
+      <NewsSideBar />
+      <Feed />
+      <RightBar />
+    </div>
+  );
+}
+export default Home;
